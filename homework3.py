52ac990a4681d387d028d44032ee896d424d4e87
yavastudent <yavastudent@gmail.com>
2024-07-21 13:30:53 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,10 @@
+name = "Name: Vladimir"
+print(name)
+age = "Age:""38"
+print(age)
+new_age ="New Age:"
+age =38+1
+print (new_age, age)
+is_student = "is student:"
+student= 1==1
+print(is_student, student)
